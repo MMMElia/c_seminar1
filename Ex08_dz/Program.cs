b353322f1b4502d604f9f239c0f0cd8de454cdb6
Elya Megrabyan <m-elia@yandex.ru>
2023-01-30 17:13:00 +0300

    Домашняя работа. Задача 8

---
+++ Ex08_dz/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число:  ");
+int n =int.Parse(Console.ReadLine());
+
+if (n<0) {
+        Console.Write("Введите положительное число:");
+        n =int.Parse(Console.ReadLine());
+}
+int i=1;
+
+while (i<=n) {
+    if (i%2==0) {Console.Write ($"{i}  ");}
+    i++;
+}
