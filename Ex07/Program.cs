be4334507d17bf785b62063ede48f85dce501b60
Elya Megrabyan <m-elia@yandex.ru>
2023-01-30 16:19:02 +0300

    Задача 07

---
+++ Ex07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число:  ");
+int number =int.Parse(Console.ReadLine());
+
+if (number<99 || number>999) {Console.Write("Вы вели неверное число. Введите трехзначное число  ");
+   number =int.Parse(Console.ReadLine());
+}
+
+int i=number % 10;
+Console.Write($"Последняя цифра числа {number} это цифра {i}");
