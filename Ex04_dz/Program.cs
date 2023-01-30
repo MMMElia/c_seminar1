d61dda9fc11fbd0dee74092db5e9eb75394c6e31
Elya Megrabyan <m-elia@yandex.ru>
2023-01-30 16:55:52 +0300

    Домашняя работа. Задача 4

---
+++ Ex04_dz/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите первое число:  ");
+int a =int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+int b=int.Parse(Console.ReadLine());
+Console.Write("Введите третье число:  ");
+int c=int.Parse(Console.ReadLine());
+
+if (a>b && a>c) {Console.Write($"max= {a}");}
+if (b>a && b>c) {Console.Write($"max= {b}");}
+if (c>a && c>b) {Console.Write($"max= {c}");}
