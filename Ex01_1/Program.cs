f44d50c4aec11d2b1aa41ffda88d10e6fc19c9de
Elya Megrabyan <m-elia@yandex.ru>
2023-01-30 14:41:34 +0300

    Задача 01

---
+++ Ex01_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число:  ");
+int a =int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+int b=int.Parse(Console.ReadLine());
+
+if (a*a==b) {
+    Console.Write("Да");
+        
+    }
+else {
+     Console.Write("Нет");
+}
